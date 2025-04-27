b688906708211b389aeaf50a41a85ae1b8b21f33
Gabriel Rdorigues dos Santos <gabrielrsantos999@gmail.com>
2025-04-27 16:00:07 -0300

    visualizando dados

---
+++ javaUdemy/jdbc2/src/programa/Programa.java
@@ -0,0 +1,30 @@
+package programa;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+
+public class Programa {
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			con = DB.getConnection();
+			st = con.createStatement();
+			rs = st.executeQuery("select * from department");
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DB.closeResultSet(rs);
+			DB.closeStatment(st);
+			DB.closeConnection();
+		}
+	}
+}
