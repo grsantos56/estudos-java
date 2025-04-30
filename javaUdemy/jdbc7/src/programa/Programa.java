475d296e006f0a7305028dc58c910588e522e81a
Gabriel Rdorigues dos Santos <gabrielrsantos999@gmail.com>
2025-04-30 08:56:00 -0300

    projeto: departamento

---
+++ javaUdemy/jdbc7/src/programa/Programa.java
@@ -0,0 +1,10 @@
+package programa;
+
+import entidades.Department;
+
+public class Programa {
+	public static void main(String[] args) {
+		Department departamento = new Department(2, "jose");
+		System.out.println(departamento);
+	}
+}
